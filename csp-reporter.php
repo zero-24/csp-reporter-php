21cbadd52bd0c813d0de76825458312a59ee63c2
zero-24 <zero-24@users.noreply.github.com>
2017-10-06 20:49:24 +0200

    add a blacklist for the google chrome bug with the translation this can also be used to add a custom blacklist

--- csp-reporter.php
+++ csp-reporter.php
@@ -2,13 +2,20 @@
 // Configuration -->
 $recipients = ['mail@example.org'];
 $subject    = 'CSP Violation on %s';
+
+// Blacklist of files to not report to you.
+$blacklist = [
+	// There is a chrome bug with the inbuild translation: https://stackoverflow.com/questions/41052219/content-security-policy-translate-googleapis-com
+	'https://www.gstatic.com/images/branding/product/2x/translate_24dp.png',
+	'https://translate.googleapis.com/translate_static/css/translateelement.css',
+];
 // <-- Configuration
 
 // Actual Code -->
 $inputData = file_get_contents('php://input');
 $jsonData  = json_decode($inputData, true);
 
-if ($jsonData)
+if ($jsonData && !in_array($jsonData['csp-report']['blocked-uri'], $blacklist))
 {
 	$mailData = json_encode(
 		$jsonData,
